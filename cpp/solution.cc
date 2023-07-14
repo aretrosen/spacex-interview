bea44241f9df7116530f846a917c25f0af2ac94a
Aritra Sen <aretrosen@proton.me>
2023-07-15 03:34:23 +0530

    (feat) final solution

--- cpp/solution.cc
+++ cpp/solution.cc
@@ -1,8 +1,11 @@
 #include "util.h"
 
 #include <algorithm>
-#include <iostream>
 #include <unordered_map>
+
+const double user_sat_angle = 45.0;
+const double sat_sat_angle = 10.0;
+const int beam_per_color = 8;
 
 std::map<User, std::pair<Sat, Color>>
    solve(const std::map<User, Vector3> &users,
@@ -16,7 +19,7 @@ std::map<User, std::pair<Sat, Color>>
    for (const auto &[sat_id, sat_loc] : sats) {
       for (const auto &[user_id, user_loc] : users) {
          auto angle = 180 - std::abs(user_loc.angle_between(center, sat_loc));
-         if (angle <= 45.0) {
+         if (angle <= user_sat_angle) {
             possible_users[sat_id].push_back({angle, user_id});
          }
       }
@@ -27,35 +30,25 @@ std::map<User, std::pair<Sat, Color>>
    for (auto &[sat_id, pos_users] : possible_users) {
       int num = pos_users.size();
       std::sort(pos_users.begin(), pos_users.end());
-      std::vector<User> valid_users;
-      valid_users.reserve(num);
-      for (int i = 0; i < num; ++i) {
-         if (user_done[pos_users[i].second]) continue;
-         valid_users.push_back(pos_users[i].second);
-      }
-      num = valid_users.size();
 
-      int l_idx[4] = {-1, -1, -1, -1};
-      int c_idx = 0;
-      int cnt_sats = 0;
-
-      for (int idx = 0; idx < num && cnt_sats < 32; ++idx) {
-         if (l_idx[c_idx] != -1 &&
-             std::abs(sats.at(sat_id).angle_between(
-                users.at(valid_users[idx]), users.at(l_idx[c_idx]))) < 10.0)
-            continue;
-
-         if (l_idx[c_idx] != -1) {
-            std::cout << std::abs(sats.at(sat_id).angle_between(
-                            users.at(valid_users[idx]), users.at(l_idx[c_idx])))
-                      << ' ' << valid_users[idx] << ' ' << l_idx[c_idx] << '\n';
+      for (char color : colors) {
+         std::vector<User> uuids;
+         for (int i = 0; i < num && uuids.size() < beam_per_color; ++i) {
+            User uuid = pos_users[i].second;
+            if (user_done[uuid]) continue;
+            bool break_condition = true;
+            for (auto old_id : uuids) {
+               if (std::abs(sats.at(sat_id).angle_between(
+                      users.at(old_id), users.at(uuid))) < sat_sat_angle) {
+                  break_condition = false;
+                  break;
+               }
+            }
+            if (!break_condition) continue;
+            uuids.push_back(uuid);
+            user_done[uuid] = true;
+            solution[uuid] = {sat_id, color};
          }
-
-         ++cnt_sats;
-         l_idx[c_idx] = valid_users[idx];
-         solution[valid_users[idx]] = {sat_id, colors[c_idx++]};
-         user_done[valid_users[idx]] = true;
-         c_idx %= 4;
       }
    }
 
